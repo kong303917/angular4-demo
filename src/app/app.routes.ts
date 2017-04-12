8c1e52a5fbbb41367e196a50a4a3eed0d17a060d
kong303917 <kongcheng_303917@sina.com>
2017-04-12 10:14:22 +0800

    init project

---
+++ src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+
+export const routes:Routes = [ // Routes类型的数组
+    {
+        path      : 'index',
+        component : AppComponent
+    },{
+        path      : '',
+        redirectTo: 'index',
+        pathMatch : 'full'
+    }
+];
